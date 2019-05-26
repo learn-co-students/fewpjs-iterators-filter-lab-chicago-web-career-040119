c0cf70397cee430c53d7989c373b7df5b5bcc225
Jackson Robbins <jackrobbins1@gmail.com>
2019-05-26 17:56:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+let findMatching = (array, name) => {
+  return array.filter(person => person.toLowerCase() === name.toLocaleLowerCase())
+}
+
+let fuzzyMatch = (array, query) => {
+  return array.filter(person => {
+    let lowName = person.toLowerCase()
+    let cutName = lowName.slice(0, query.length)
+    return cutName === query.toLowerCase()
+  })
+}
+
+let matchName = (array, person) => {
+  return array.filter(obj => {
+    return obj.name === person
+  })
+}
